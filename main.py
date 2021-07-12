61ba9fc2762d15efc35ad1e760a2b852ed369bee
Patrick <56480973+PatrickTLuu@users.noreply.github.com>
2021-07-12 10:56:10 -0400

    Add html import to main app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import import_html as myHTML
 from mysql.connector.errors import DatabaseError
 
 
@@ -10,16 +11,16 @@ def setup():
     try:
         mydb = mysql.connector.connect(
             host="localhost", 
-            user="INSERT USER", 
-            password="INSERT PASSWORD", 
+            user="USERNAME", 
+            password="PASSWORD", 
             database="bookmarks")
         myCursor = mydb.cursor()
 
     except DatabaseError:
         mydb = mysql.connector.connect(
             host="localhost", 
-            user="INSERT USER", 
-            password="INSERT PASSWORD")
+            user="USERNAME", 
+            password="PASSWORD")
 
         myCursor = mydb.cursor()
         myCursor.execute("CREATE DATABASE bookmarks")
@@ -30,7 +31,7 @@ def setup():
 
 # Get all bookmarks and prints them
 def getBookmarks():
-    myCursor.execute("SELECT * FROM bookmarks ORDER BY name")
+    myCursor.execute("SELECT * FROM bookmarks ORDER BY id")
     bookmarks = myCursor.fetchall()
     for x in bookmarks:
         print(x)
@@ -43,7 +44,7 @@ def addBookmark():
     url = str(input("Enter url of bookmark: "))
     notes = str(input("Notes to add to bookmark: "))
 
-    val = (name, url, notes)
+    val = cleanBookmark(name, url, notes)
     myCursor.execute(sql, val)
     mydb.commit()
     print("\nBookmark Added.")
@@ -63,19 +64,63 @@ def deleteBookmark():
         print("\nCanceling Delete.")
 
 
+#Imports bookmarks from html
+def importBookmarks():
+    error = []
+    parser = myHTML.BookmarkHTMLParser()
+    file = "C:/Users/patri.LAPTOP-GHL2QTQE/Documents/Bookmarks.html"
+    bookmarks = open(file).read()
+    parser.feed(bookmarks)
+
+
+    sql = "INSERT INTO bookmarks (name, url, notes) VALUES (%s, %s, %s)"
+    for bookmark in myHTML.allBookmarks:
+        name = bookmark[1]
+        url = bookmark[0][1]
+        notes = ""
+
+        val = cleanBookmark(name, url, notes)
+        search = val[0].replace(" ", "_")
+
+        myCursor.execute("SELECT id FROM bookmarks WHERE name LIKE '%s'" % search)
+        findBookmark = myCursor.fetchall()
+
+        if findBookmark == []:
+            try:
+                myCursor.execute(sql, val)
+                mydb.commit()
+            
+            except mysql.connector.errors.DataError:
+                error.append(val)
+
+
+    print("\n%s bookmarks imported." % str(len(myHTML.allBookmarks)+1))
+    print("Unable to import: %s" % error)
+
+
+#Clean bookmarks
+def cleanBookmark(name, url, notes):
+    cleanName = name.replace("'", "")
+    cleanUrl = url.split("?", 1)[0]
+    CleanNotes = notes.replace("'", "")
+    return (cleanName, cleanUrl, CleanNotes)
+
+
+
 if __name__ == '__main__':
     active = True
     actions = {
         "list bookmarks": getBookmarks,
         "add bookmark": addBookmark,
         "delete bookmark": deleteBookmark,
+        "import bookmarks": importBookmarks,
         "exit": exit
     }
     setup()
 
     while active:
         try:
-            print("\n\nActions avalible: List Bookmarks, Add Bookmark, Delete Bookmark, Exit")
+            print("\n\nActions avalible: List Bookmarks, Add Bookmark, Delete Bookmark, Import Bookmarks, Exit")
             action = str(input(">>> ")).lower()
             actions[action]()
 
